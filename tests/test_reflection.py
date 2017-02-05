774cd56064cdb5405378fa16c753c30c9f6fd453
Josh Meyers <jmeyers314@gmail.com>
2017-02-05 01:27:28 -0800

    Add asphere reflection unit tests

--- tests/test_reflection.py
+++ tests/test_reflection.py
@@ -135,9 +135,66 @@ def test_paraboloid_reflection_to_focus():
             assert isclose(focus.z, 1.0/(4*A), abs_tol=1e-12)
 
 
+def test_asphere_reflection_plane():
+    import random
+    asphere = jtrace.Asphere(25.0, -0.97, [1e-3, 1e-5], 0.1)
+    for i in range(1000):
+        x = random.gauss(0, 1)
+        y = random.gauss(0, 1)
+        vx = random.gauss(0, 1e-1)
+        vy = random.gauss(0, 1e-1)
+        ray = jtrace.Ray(x, y, 0, vx, vy, 1, 0)
+        isec = asphere.intersect(ray)
+        rray = isec.reflectedRay(ray)
+
+        # ray.v, surfaceNormal, and rray.v should all be in the same plane, and
+        # hence (ray.v x surfaceNormal) . rray.v should have zero magnitude.
+        # magnitude zero.
+        assert isclose(
+            jtrace.DotProduct(
+                jtrace.CrossProduct(ray.v, isec.surfaceNormal),
+                rray.v),
+            0.0, rel_tol=0, abs_tol=1e-15)
+
+
+def test_asphere_reflection_reversal():
+    import random
+    asphere = jtrace.Asphere(23.0, -0.97, [1e-5, 1e-6], 0.1)
+    for i in range(1000):
+        x = random.gauss(0, 1)
+        y = random.gauss(0, 1)
+        vx = random.gauss(0, 1e-1)
+        vy = random.gauss(0, 1e-1)
+        ray = jtrace.Ray(jtrace.Vec3(x, y, 0),
+                         jtrace.Vec3(vx, vy, 1).UnitVec3(),
+                         0)
+        isec = asphere.intersect(ray)
+        rray = isec.reflectedRay(ray)
+
+        # Invert the reflected ray, and see that it ends back at the starting
+        # point
+
+        # Keep going a bit before turning around though
+        turn_around = rray(rray.t0+0.1)
+        return_ray = jtrace.Ray(turn_around, -rray.v, -(rray.t0+0.1))
+        risec = asphere.intersect(return_ray)
+        # First check that we intersected at the same point
+        assert isclose(isec.point.x, risec.point.x, rel_tol=0, abs_tol=1e-9)
+        assert isclose(isec.point.y, risec.point.y, rel_tol=0, abs_tol=1e-9)
+        assert isclose(isec.point.z, risec.point.z, rel_tol=0, abs_tol=1e-9)
+        # Reflect and propagate back to t=0.
+        cray = risec.reflectedRay(return_ray)
+        cray = cray(0)
+        assert isclose(cray.x, x, rel_tol=0, abs_tol=1e-9)
+        assert isclose(cray.y, y, rel_tol=0, abs_tol=1e-9)
+        assert isclose(cray.z, 0, rel_tol=0, abs_tol=1e-9)
+
+
 if __name__ == '__main__':
     test_plane_reflection_plane()
     test_plane_reflection_reversal()
     test_paraboloid_reflection_plane()
     test_paraboloid_reflection_reversal()
     test_paraboloid_reflection_to_focus()
+    test_asphere_reflection_plane()
+    test_asphere_reflection_reversal()
