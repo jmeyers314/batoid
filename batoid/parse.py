e38e5bde994f5ef0e9dfb66da90c2a58afac4da6
Josh Meyers <jmeyers314@gmail.com>
2017-10-23 15:14:06 -0700

    Add more parsing

--- batoid/parse.py
+++ batoid/parse.py
@@ -16,3 +16,45 @@ def parse_obscuration(config):
         # put type back into config, but with Clear->Obsc
         config['type'] = typ.replace("Clear", "Obsc")
         return batoid._batoid.ObscNegation(parse_obscuration(config))
+
+
+def parse_surface(config):
+    typ = config.pop('type')
+    evalstr = "batoid.{}(B=0.0, **config)".format(typ)
+    return eval(evalstr)
+
+
+def parse_coordSys(config):
+    if any(x in config for x in ['x', 'y', 'z']):
+        if 'shift' in config:
+            raise ValueError("Cannot specify both shift and x/y/z")
+        x = config.pop('x', 0.0)
+        y = config.pop('y', 0.0)
+        z = config.pop('z', 0.0)
+        shift = [x, y, z]
+    elif 'shift' in config:
+        shift = config.pop('shift')
+    # Leaving rotation out for the moment...
+    coordSys = batoid._batoid.CoordSys()
+    if shift != [0.0, 0.0, 0.0]:
+        coordSys = coordSys.shiftLocal(batoid.Vec3(shift))
+    return coordSys
+
+def parse_optic(config):
+    if 'obscuration' in config:
+        obscuration = parse_obscuration(config.pop('obscuration'))
+    else:
+        obscuration = None
+    name = config.pop('name', "")
+    if 'coordSys' in 'config':
+        coordSys = parse_coordSys(config.pop('coordSys'))
+    else:
+        coordSys = batoid._batoid.CoordSys()
+
+    # Need inMedium, outMedium.  inherited from parent most likely.
+
+    typ = config.pop('type')
+    if typ == 'Mirror':
+        surface = parse_surface(config.pop('surface'))
+
+        return batoid.optic.Mirror(surface, name=name, coordSys=coordSys, obscuration=obscuration)
