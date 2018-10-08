2a0ab2f33e4ce79c9bd55215e6258f63c7b3cdf8
Josh Meyers <jmeyers314@gmail.com>
2018-10-08 16:41:46 -0700

    Make GCC happy

--- pysrc/coating.cpp
+++ pysrc/coating.cpp
@@ -10,14 +10,14 @@ namespace batoid {
     void pyExportCoating(py::module& m) {
         py::class_<Coating, std::shared_ptr<Coating>>(m, "Coating")
             .def("getCoefs", &Coating::getCoefs)
-            .def(py::self == py::self)
-            .def(py::self != py::self)
             .def("__repr__", &Coating::repr);
 
         py::class_<SimpleCoating, std::shared_ptr<SimpleCoating>, Coating>(m, "SimpleCoating")
             .def(py::init<double,double>(), "init", "reflectivity"_a, "transmissivity"_a)
             .def_readonly("reflectivity", &SimpleCoating::_reflectivity)
             .def_readonly("transmissivity", &SimpleCoating::_transmissivity)
+            .def(py::self == py::self)
+            .def(py::self != py::self)
             .def(py::pickle(
                 [](const SimpleCoating& sc){
                     return py::make_tuple(sc._reflectivity, sc._transmissivity);
