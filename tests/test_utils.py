2aab23e2f3467a23ee83de093900777df2762bbc
Josh Meyers <jmeyers314@gmail.com>
2019-04-25 15:00:11 -0700

    Add Postel tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -68,6 +68,39 @@ def test_gnomonicDirCos():
 
 
 @timer
+def test_postelDirCos():
+    np.random.seed(5)
+    u = np.random.uniform(-0.1, 0.1, size=1000)
+    v = np.random.uniform(-0.1, 0.1, size=1000)
+
+    # Test round trip
+    u1, v1 = batoid.utils.dirCosToPostel(*batoid.utils.postelToDirCos(u, v))
+    np.testing.assert_allclose(u, u1, rtol=1e-10, atol=1e-12)
+    np.testing.assert_allclose(v, v1, rtol=1e-10, atol=1e-12)
+
+    # Test round trip in the other direction
+    alpha = np.random.uniform(-0.1, 0.1, size=1000)
+    beta = np.random.uniform(-0.1, 0.1, size=1000)
+    gamma = np.sqrt(1 - alpha**2 - beta**2)
+    alpha1, beta1, gamma1 = batoid.utils.postelToDirCos(
+        *batoid.utils.dirCosToPostel(alpha, beta, gamma)
+    )
+    np.testing.assert_allclose(alpha, alpha1, rtol=1e-10, atol=1e-12)
+    np.testing.assert_allclose(beta, beta1, rtol=1e-10, atol=1e-12)
+    np.testing.assert_allclose(gamma, gamma1, rtol=1e-10, atol=1e-12)
+
+    # For really tiny angles, u/v should be basically the same as alpha/beta
+    u = np.random.uniform(-1e-6, 1e-6, size=1000)
+    v = np.random.uniform(-1e-6, 1e-6, size=1000)
+    alpha, beta, gamma = batoid.utils.postelToDirCos(u, v)
+    np.testing.assert_allclose(alpha, u, rtol=0, atol=1e-8)
+    np.testing.assert_allclose(beta, v, rtol=0, atol=1e-8)
+
+    # Check normalization of direction cosines
+    np.testing.assert_allclose(np.sqrt(alpha*alpha+beta*beta+gamma*gamma), 1, rtol=0, atol=1e-15)
+
+
+@timer
 def test_gnomonicSpherical():
     np.random.seed(57)
     u = np.random.uniform(-0.1, 0.1, size=1000)
@@ -257,6 +290,7 @@ def test_jacobian():
 if __name__ == '__main__':
     test_normalized()
     test_gnomonicDirCos()
+    test_postelDirCos()
     test_gnomonicSpherical()
     test_sphericalToDirCos()
     test_composition()
