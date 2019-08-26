3485eb7c4612793717c292028a76dd6a6a86cb9b
Josh Meyers <jmeyers314@gmail.com>
2019-08-26 08:30:17 -0700

    GQ of vignetted rays raises exception

--- batoid/analysis.py
+++ batoid/analysis.py
@@ -202,6 +202,9 @@ def zernikeGQ(optic, theta_x, theta_y, wavelength, nrings=6, nspokes=None,
         sphereRadius = optic.sphereRadius
 
     optic.traceInPlace(rays, outCoordSys=batoid.globalCoordSys)
+    # if np.any(rays.failed):
+    if np.any(rays.vignetted):
+        raise ValueError("Cannot compute zernike with Gaussian Quadrature with failed rays.")
     if reference == 'mean':
         w = np.where(1-rays.vignetted)[0]
         point = np.mean(rays.r[w], axis=0)
