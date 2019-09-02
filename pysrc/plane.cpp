0c5af6f325cc1ca03a71b298c07008a55934a82c
Josh Meyers <jmeyers314@gmail.com>
2019-09-02 10:36:55 -0700

    Optionally allow reverse tracing to Planes

--- pysrc/plane.cpp
+++ pysrc/plane.cpp
@@ -11,6 +11,7 @@ using namespace pybind11::literals;
 namespace batoid {
     void pyExportPlane(py::module& m) {
         py::class_<Plane, std::shared_ptr<Plane>, Surface>(m, "Plane")
-            .def(py::init<>(), "init");
+            .def(py::init<bool>(), "init", "allowReverse"_a=false)
+            .def_property_readonly("allowReverse", &Plane::getAllowReverse);
     }
 }
