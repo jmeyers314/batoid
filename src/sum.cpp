67e158af6526f5c91132a3f3d1d174fb7f0c563a
Josh Meyers <jmeyers314@gmail.com>
2020-01-31 09:31:07 -0800

    Improve intersection algorithm for Sum

--- src/sum.cpp
+++ src/sum.cpp
@@ -25,34 +25,42 @@ namespace batoid {
         return result.normalized();
     }
 
-    class SumResidual {
-    public:
-        SumResidual(const Sum& s, const Ray& r) : _s(s), _r(r) {}
-        double operator()(double t) const {
-            Vector3d p = _r.positionAtTime(t);
-            return _s.sag(p(0), p(1)) - p(2);
-        }
-    private:
-        const Sum& _s;
-        const Ray& _r;
-    };
+    // class SumResidual {
+    // public:
+    //     SumResidual(const Sum& s, const Ray& r) : _s(s), _r(r) {}
+    //     double operator()(double t) const {
+    //         Vector3d p = _r.positionAtTime(t);
+    //         return _s.sag(p(0), p(1)) - p(2);
+    //     }
+    // private:
+    //     const Sum& _s;
+    //     const Ray& _r;
+    // };
+    //
+    // bool Sum::timeToIntersect(const Ray& r, double& t) const {
+    //     // Use first surface as an initial guess
+    //     if (!_surfaces[0]->timeToIntersect(r, t))
+    //         return false;
+    //     SumResidual resid(*this, r);
+    //     Solve<SumResidual> solve(resid, t, t+1e-2);
+    //     solve.setMethod(Method::Brent);
+    //     solve.setXTolerance(1e-12);
+    //
+    //     try {
+    //         solve.bracket();
+    //         t = solve.root();
+    //     } catch (const SolveError&) {
+    //         return false;
+    //     }
+    //     if (t < r.t) return false;
+    //     return true;
+    // }
 
     bool Sum::timeToIntersect(const Ray& r, double& t) const {
         // Use first surface as an initial guess
         if (!_surfaces[0]->timeToIntersect(r, t))
             return false;
-        SumResidual resid(*this, r);
-        Solve<SumResidual> solve(resid, t, t+1e-2);
-        solve.setMethod(Method::Brent);
-        solve.setXTolerance(1e-12);
-
-        try {
-            solve.bracket();
-            t = solve.root();
-        } catch (const SolveError&) {
-            return false;
-        }
-        if (t < r.t) return false;
-        return true;
+        bool success = Surface::timeToIntersect(r, t);
+        return (success && t >= r.t);
     }
 }
