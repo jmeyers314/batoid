158127108952e50e7dd5355f39e5bf1ca5107fcf
Josh Meyers <jmeyers314@gmail.com>
2020-02-19 10:39:19 -0800

    merge GPU transform/intersection/reflection

--- src/plane2.cpp
+++ src/plane2.cpp
@@ -55,15 +55,18 @@ namespace batoid {
                 double vz = vxptr[i]*rot[6] + vyptr[i]*rot[7] + vzptr[i]*rot[8];
                 double t = tptr[i];
                 if (!failptr[i]) {
+                    // intersection
                     double dt = -z/vz;
                     if (!_allowReverse && dt < 0) {
                         failptr[i] = true;
                         vigptr[i] = true;
                     } else {
+                        // propagation
                         x += vx * dt;
                         y += vy * dt;
                         z += vz * dt;
                         t += dt;
+                        // output
                         xptr[i] = x;
                         yptr[i] = y;
                         zptr[i] = z;
@@ -79,26 +82,87 @@ namespace batoid {
     }
 
     void Plane2::_reflectInPlace(RayVector2& rv, const CoordSys* cs) const {
-        _intersectInPlace(rv, cs);
-        size_t size = rv.size;
-        double* vzptr = rv.v.deviceData+2*size;
+        rv.r.syncToDevice();
+        rv.v.syncToDevice();
+        rv.t.syncToDevice();
+        rv.vignetted.syncToDevice();
+        rv.failed.syncToDevice();
 
-        #pragma omp target is_device_ptr(vzptr)
-        {
-            #pragma omp teams distribute parallel for
-            for(int i=0; i<size; i++) {
-                vzptr[i] *= -1;
-            }
-        }
-    }
-
-    void Plane2::_refractInPlace(RayVector2& rv, const Medium2& m1, const Medium2& m2, const CoordSys* cs) const {
-        intersectInPlace(rv, cs);
         size_t size = rv.size;
+        double* xptr = rv.r.deviceData;
+        double* yptr = xptr + size;
+        double* zptr = yptr + size;
         double* vxptr = rv.v.deviceData;
         double* vyptr = vxptr + size;
         double* vzptr = vyptr + size;
-        double* wptr = rv.wavelength.deviceData;
+        double* tptr = rv.t.deviceData;
+        bool* vigptr = rv.vignetted.deviceData;
+        bool* failptr = rv.failed.deviceData;
+        if (!cs)
+            cs = &rv.getCoordSys();
+        CoordTransform2 ct(rv.getCoordSys(), *cs);
+        const double* rot = ct.getRot().data();
+        const double* dr = ct.getDr().data();
+        #pragma omp target is_device_ptr(xptr, yptr, zptr, vxptr, vyptr, vzptr, tptr, vigptr, failptr) map(to:rot[:9],dr[:3])
+        {
+            #pragma omp teams distribute parallel for
+            for(int i=0; i<size; i++) {
+                // Coordinate transformation
+                double x = (xptr[i]-dr[0])*rot[0] + (yptr[i]-dr[1])*rot[1] + (zptr[i]-dr[2])*rot[2];
+                double y = (xptr[i]-dr[0])*rot[3] + (yptr[i]-dr[1])*rot[4] + (zptr[i]-dr[2])*rot[5];
+                double z = (xptr[i]-dr[0])*rot[6] + (yptr[i]-dr[1])*rot[7] + (zptr[i]-dr[2])*rot[8];
+                double vx = vxptr[i]*rot[0] + vyptr[i]*rot[1] + vzptr[i]*rot[2];
+                double vy = vxptr[i]*rot[3] + vyptr[i]*rot[4] + vzptr[i]*rot[5];
+                double vz = vxptr[i]*rot[6] + vyptr[i]*rot[7] + vzptr[i]*rot[8];
+                double t = tptr[i];
+                if (!failptr[i]) {
+                    // intersection
+                    double dt = -z/vz;
+                    if (!_allowReverse && dt < 0) {
+                        failptr[i] = true;
+                        vigptr[i] = true;
+                    } else {
+                        // propagation
+                        x += vx * dt;
+                        y += vy * dt;
+                        z += vz * dt;
+                        t += dt;
+                        // output
+                        xptr[i] = x;
+                        yptr[i] = y;
+                        zptr[i] = z;
+                        vxptr[i] = vx;
+                        vyptr[i] = vy;
+                        vzptr[i] = -vz; // reflect!
+                        tptr[i] = t;
+                    }
+                }
+            }
+        }
+        rv.setCoordSys(CoordSys(*cs));
+    }
+
+    void Plane2::_refractInPlace(RayVector2& rv, const Medium2& m1, const Medium2& m2, const CoordSys* cs) const {
+        rv.r.syncToDevice();
+        rv.v.syncToDevice();
+        rv.t.syncToDevice();
+        rv.vignetted.syncToDevice();
+        rv.failed.syncToDevice();
+        size_t size = rv.size;
+        double* xptr = rv.r.deviceData;
+        double* yptr = xptr + size;
+        double* zptr = yptr + size;
+        double* vxptr = rv.v.deviceData;
+        double* vyptr = vxptr + size;
+        double* vzptr = vyptr + size;
+        double* tptr = rv.t.deviceData;
+        bool* vigptr = rv.vignetted.deviceData;
+        bool* failptr = rv.failed.deviceData;
+        if (!cs)
+            cs = &rv.getCoordSys();
+        CoordTransform2 ct(rv.getCoordSys(), *cs);
+        const double* rot = ct.getRot().data();
+        const double* dr = ct.getDr().data();
 
         // DualView<double> n1(size);
         // double* n1ptr = n1.deviceData;
@@ -107,27 +171,58 @@ namespace batoid {
         double* n2ptr = n2.deviceData;
         m2.getNMany(rv.wavelength, n2);
 
-        #pragma omp target is_device_ptr(n2ptr, vxptr, vyptr, vzptr)
+        #pragma omp target is_device_ptr(xptr, yptr, zptr, vxptr, vyptr, vzptr, n2ptr, tptr, vigptr, failptr) map(to:rot[:9],dr[:3])
         {
             #pragma omp teams distribute parallel for
             for(int i=0; i<size; i++) {
-                double n1 = vxptr[i]*vxptr[i];
-                n1 += vyptr[i]*vyptr[i];
-                n1 += vzptr[i]*vzptr[i];
-                n1 = 1/sqrt(n1);
+                // Coordinate transformation
+                double x = (xptr[i]-dr[0])*rot[0] + (yptr[i]-dr[1])*rot[1] + (zptr[i]-dr[2])*rot[2];
+                double y = (xptr[i]-dr[0])*rot[3] + (yptr[i]-dr[1])*rot[4] + (zptr[i]-dr[2])*rot[5];
+                double z = (xptr[i]-dr[0])*rot[6] + (yptr[i]-dr[1])*rot[7] + (zptr[i]-dr[2])*rot[8];
+                double vx = vxptr[i]*rot[0] + vyptr[i]*rot[1] + vzptr[i]*rot[2];
+                double vy = vxptr[i]*rot[3] + vyptr[i]*rot[4] + vzptr[i]*rot[5];
+                double vz = vxptr[i]*rot[6] + vyptr[i]*rot[7] + vzptr[i]*rot[8];
+                double t = tptr[i];
+                // intersection
+                if (!failptr[i]) {
+                    // intersection
+                    double dt = -z/vz;
+                    if (!_allowReverse && dt < 0) {
+                        failptr[i] = true;
+                        vigptr[i] = true;
+                    } else {
+                        // propagation
+                        x += vx * dt;
+                        y += vy * dt;
+                        z += vz * dt;
+                        t += dt;
+                        // refraction
+                        double n1 = vx*vx;
+                        n1 += vy*vy;
+                        n1 += vz*vz;
+                        n1 = 1/sqrt(n1);
 
-                double discriminant = vzptr[i]*vzptr[i] * n1*n1;
-                discriminant -= (1-n2ptr[i]*n2ptr[i]/(n1*n1));
+                        double discriminant = vz*vz * n1*n1;
+                        discriminant -= (1-n2ptr[i]*n2ptr[i]/(n1*n1));
 
-                double norm = n1*n1*vxptr[i]*vxptr[i];
-                norm += n1*n1*vyptr[i]*vyptr[i];
-                norm += discriminant;
-                norm = sqrt(norm);
-                vxptr[i] = n1*vxptr[i]/norm/n2ptr[i];
-                vyptr[i] = n1*vyptr[i]/norm/n2ptr[i];
-                vzptr[i] = sqrt(discriminant)/norm/n2ptr[i];
+                        double norm = n1*n1*vx*vx;
+                        norm += n1*n1*vy*vy;
+                        norm += discriminant;
+                        norm = sqrt(norm);
+                        vxptr[i] = n1*vx/norm/n2ptr[i];
+                        vyptr[i] = n1*vy/norm/n2ptr[i];
+                        vzptr[i] = sqrt(discriminant)/norm/n2ptr[i];
+
+                        // output
+                        xptr[i] = x;
+                        yptr[i] = y;
+                        zptr[i] = z;
+                        tptr[i] = t;
+                    }
+                }
             }
         }
+        rv.setCoordSys(CoordSys(*cs));
     }
 
     // Specializations
--- src/surface2.cpp
+++ src/surface2.cpp
@@ -66,7 +66,6 @@ namespace batoid {
                 if (!failptr[i]) {
                     double dt;
                     bool success = self->_timeToIntersect(x, y, z, vx, vy, vz, dt);
-                    // output
                     if (success && dt >= 0) {
                         x += vx * dt;
                         y += vy * dt;
@@ -92,47 +91,102 @@ namespace batoid {
     template<typename T>
     void Surface2CRTP<T>::reflectInPlace(RayVector2& rv, const CoordSys* cs) const {
         const T* self = static_cast<const T*>(this);
-        self->intersectInPlace(rv, cs);
         rv.r.syncToDevice();  // should be redundant...
         rv.v.syncToDevice();
+        rv.t.syncToDevice();
+        rv.vignetted.syncToDevice();
+        rv.failed.syncToDevice();
         size_t size = rv.size;
         double* xptr = rv.r.deviceData;
         double* yptr = xptr + size;
+        double* zptr = yptr + size;
         double* vxptr = rv.v.deviceData;
         double* vyptr = vxptr + size;
         double* vzptr = vyptr + size;
+        double* tptr = rv.t.deviceData;
+        bool* vigptr = rv.vignetted.deviceData;
+        bool* failptr = rv.failed.deviceData;
+        if (!cs)
+            cs = &rv.getCoordSys();
+        CoordTransform2 ct(rv.getCoordSys(), *cs);
+        const double* rot = ct.getRot().data();
+        const double* dr = ct.getDr().data();
 
-        #pragma omp target is_device_ptr(xptr, yptr, vxptr, vyptr, vzptr) map(to:self[:1])
+        #pragma omp target is_device_ptr(xptr, yptr, zptr, vxptr, vyptr, vzptr, tptr, vigptr, failptr) map(to:self[:1]) map(to:rot[:9],dr[:3])
         {
             #pragma omp teams distribute parallel for
             for(int i=0; i<size; i++) {
-                // get surface normal vector normVec
-                double normalx, normaly, normalz;
-                self->_normal(xptr[i], yptr[i], normalx, normaly, normalz);
-                // alpha = v dot normVec
-                double alpha = vxptr[i]*normalx;
-                alpha += vyptr[i]*normaly;
-                alpha += vzptr[i]*normalz;
-                // v -= 2 alpha normVec
-                vxptr[i] -= 2*alpha*normalx;
-                vyptr[i] -= 2*alpha*normaly;
-                vzptr[i] -= 2*alpha*normalz;
+                // Coordinate transformation
+                double x = (xptr[i]-dr[0])*rot[0] + (yptr[i]-dr[1])*rot[1] + (zptr[i]-dr[2])*rot[2];
+                double y = (xptr[i]-dr[0])*rot[3] + (yptr[i]-dr[1])*rot[4] + (zptr[i]-dr[2])*rot[5];
+                double z = (xptr[i]-dr[0])*rot[6] + (yptr[i]-dr[1])*rot[7] + (zptr[i]-dr[2])*rot[8];
+                double vx = vxptr[i]*rot[0] + vyptr[i]*rot[1] + vzptr[i]*rot[2];
+                double vy = vxptr[i]*rot[3] + vyptr[i]*rot[4] + vzptr[i]*rot[5];
+                double vz = vxptr[i]*rot[6] + vyptr[i]*rot[7] + vzptr[i]*rot[8];
+                double t = tptr[i];
+                if (!failptr[i]) {
+                    double dt;
+                    // intersection
+                    bool success = self->_timeToIntersect(x, y, z, vx, vy, vz, dt);
+                    if (success && dt >= 0) {
+                        // propagation
+                        x += vx * dt;
+                        y += vy * dt;
+                        z += vz * dt;
+                        t += dt;
+                        // reflection
+                        // get surface normal vector normVec
+                        double normalx, normaly, normalz;
+                        self->_normal(x, y, normalx, normaly, normalz);
+                        // alpha = v dot normVec
+                        double alpha = vx*normalx;
+                        alpha += vy*normaly;
+                        alpha += vz*normalz;
+                        // v -= 2 alpha normVec
+                        vx -= 2*alpha*normalx;
+                        vy -= 2*alpha*normaly;
+                        vz -= 2*alpha*normalz;
+                        // output
+                        xptr[i] = x;
+                        yptr[i] = y;
+                        zptr[i] = z;
+                        vxptr[i] = vx;
+                        vyptr[i] = vy;
+                        vzptr[i] = vz;
+                        tptr[i] = t;
+                    } else {
+                        failptr[i] = true;
+                        vigptr[i] = true;
+                    }
+                }
             }
         }
+        rv.setCoordSys(CoordSys(*cs));
     }
 
     template<typename T>
     void Surface2CRTP<T>::refractInPlace(RayVector2& rv, const Medium2& m1, const Medium2& m2, const CoordSys* cs) const {
         const T* self = static_cast<const T*>(this);
-        self->intersectInPlace(rv, cs);
         rv.r.syncToDevice();  // should be redundant...
         rv.v.syncToDevice();
+        rv.t.syncToDevice();
+        rv.vignetted.syncToDevice();
+        rv.failed.syncToDevice();
         size_t size = rv.size;
         double* xptr = rv.r.deviceData;
         double* yptr = xptr + size;
+        double* zptr = yptr + size;
         double* vxptr = rv.v.deviceData;
         double* vyptr = vxptr + size;
         double* vzptr = vyptr + size;
+        double* tptr = rv.t.deviceData;
+        bool* vigptr = rv.vignetted.deviceData;
+        bool* failptr = rv.failed.deviceData;
+        if (!cs)
+            cs = &rv.getCoordSys();
+        CoordTransform2 ct(rv.getCoordSys(), *cs);
+        const double* rot = ct.getRot().data();
+        const double* dr = ct.getDr().data();
         // Note, n1 implicitly defined by rv.v.
         // DualView<double> n1(size);
         // double* n1ptr = n1.deviceData;
@@ -141,40 +195,70 @@ namespace batoid {
         double* n2ptr = n2.deviceData;
         m2.getNMany(rv.wavelength, n2);
 
-        #pragma omp target is_device_ptr(xptr, yptr, vxptr, vyptr, vzptr, n2ptr) map(to:self[:1])
+        #pragma omp target is_device_ptr(xptr, yptr, zptr, vxptr, vyptr, vzptr, n2ptr, tptr, vigptr, failptr) map(to:self[:1]) map(to:rot[:9],dr[:3])
         {
             #pragma omp teams distribute parallel for
             for(int i=0; i<size; i++) {
-                double n1 = vxptr[i]*vxptr[i];
-                n1 += vyptr[i]*vyptr[i];
-                n1 += vzptr[i]*vzptr[i];
-                n1 = 1/sqrt(n1);
-                double nvx = vxptr[i]*n1;
-                double nvy = vyptr[i]*n1;
-                double nvz = vzptr[i]*n1;
-                double normalx, normaly, normalz;
-                self->_normal(xptr[i], yptr[i], normalx, normaly, normalz);
-                double alpha = nvx*normalx;
-                alpha += nvy*normaly;
-                alpha += nvz*normalz;
-                if (alpha > 0.) {
-                    normalx *= -1;
-                    normaly *= -1;
-                    normalz *= -1;
-                    alpha *= -1;
+                // Coordinate transformation
+                double x = (xptr[i]-dr[0])*rot[0] + (yptr[i]-dr[1])*rot[1] + (zptr[i]-dr[2])*rot[2];
+                double y = (xptr[i]-dr[0])*rot[3] + (yptr[i]-dr[1])*rot[4] + (zptr[i]-dr[2])*rot[5];
+                double z = (xptr[i]-dr[0])*rot[6] + (yptr[i]-dr[1])*rot[7] + (zptr[i]-dr[2])*rot[8];
+                double vx = vxptr[i]*rot[0] + vyptr[i]*rot[1] + vzptr[i]*rot[2];
+                double vy = vxptr[i]*rot[3] + vyptr[i]*rot[4] + vzptr[i]*rot[5];
+                double vz = vxptr[i]*rot[6] + vyptr[i]*rot[7] + vzptr[i]*rot[8];
+                double t = tptr[i];
+                // intersection
+                if (!failptr[i]) {
+                    double dt;
+                    bool success = self->_timeToIntersect(x, y, z, vx, vy, vz, dt);
+                    // output
+                    if (success && dt >= 0) {
+                        // propagation
+                        x += vx * dt;
+                        y += vy * dt;
+                        z += vz * dt;
+                        t += dt;
+                        // refraction
+                        double n1 = vx*vx;
+                        n1 += vy*vy;
+                        n1 += vz*vz;
+                        n1 = 1/sqrt(n1);
+                        double nvx = vx*n1;
+                        double nvy = vy*n1;
+                        double nvz = vz*n1;
+                        double normalx, normaly, normalz;
+                        self->_normal(x, y, normalx, normaly, normalz);
+                        double alpha = nvx*normalx;
+                        alpha += nvy*normaly;
+                        alpha += nvz*normalz;
+                        if (alpha > 0.) {
+                            normalx *= -1;
+                            normaly *= -1;
+                            normalz *= -1;
+                            alpha *= -1;
+                        }
+                        double eta = n1/n2ptr[i];
+                        double sinsqr = eta*eta*(1-alpha*alpha);
+                        double nfactor = eta*alpha + sqrt(1-sinsqr);
+                        // output
+                        vxptr[i] = eta*nvx - nfactor*normalx;
+                        vyptr[i] = eta*nvy - nfactor*normaly;
+                        vzptr[i] = eta*nvz - nfactor*normalz;
+                        vxptr[i] /= n2ptr[i];
+                        vyptr[i] /= n2ptr[i];
+                        vzptr[i] /= n2ptr[i];
+                        xptr[i] = x;
+                        yptr[i] = y;
+                        zptr[i] = z;
+                        tptr[i] = t;
+                    } else {
+                        failptr[i] = true;
+                        vigptr[i] = true;
+                    }
                 }
-                double eta = n1/n2ptr[i];
-                double sinsqr = eta*eta*(1-alpha*alpha);
-                double nfactor = eta*alpha + sqrt(1-sinsqr);
-
-                vxptr[i] = eta*nvx - nfactor*normalx;
-                vyptr[i] = eta*nvy - nfactor*normaly;
-                vzptr[i] = eta*nvz - nfactor*normalz;
-                vxptr[i] /= n2ptr[i];
-                vyptr[i] /= n2ptr[i];
-                vzptr[i] /= n2ptr[i];
             }
         }
+        rv.setCoordSys(CoordSys(*cs));
     }
 
     // Instantiations
