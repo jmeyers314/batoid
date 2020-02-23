9531a510699e2bba4d95e67d9328308e42344492
Josh Meyers <jmeyers314@gmail.com>
2020-02-23 08:49:33 -0800

    rSplit returns individual ghosts

--- demo/demoSplit.py
+++ demo/demoSplit.py
@@ -5,11 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-telescope = batoid.Optic.fromYaml("LSST_g.yaml")
-factor = 0.2/1e-5/3600
+# telescope = batoid.Optic.fromYaml("LSST_g.yaml")
+# factor = 0.2/1e-5/3600
 
-# telescope = batoid.Optic.fromYaml("HSC.yaml")
-# factor = 0.168/1.5e-5/3600
+telescope = batoid.Optic.fromYaml("HSC.yaml")
+factor = 0.168/1.5e-5/3600
 
 # telescope = batoid.Optic.fromYaml("DECam.yaml")
 # factor = 0.27/1.5e-5/3600
@@ -32,25 +32,31 @@ for angle in angles:
         telescope.pupilSize/2,
         telescope.pupilSize*telescope.pupilObscuration/2,
         dirCos[0], dirCos[1], dirCos[2],
+        # 30, 90, 500e-9, 1.0, telescope.inMedium
         300, 900, 500e-9, 1.0, telescope.inMedium
     )
 
     rForward, rReverse = telescope.traceSplit(rays, minFlux=1e-4, _verbose=True)
 
     print("# input rays          = {}".format(len(rays)))
-    print("# forward output rays = {}".format(len(rForward)))
-    print("# reverse output rays = {}".format(len(rReverse)))
+    print("# forward output rays = {}".format(sum(len(rr) for rr in rForward)))
+    print("# reverse output rays = {}".format(sum(len(rr) for rr in rReverse)))
     print("input flux          = {}".format(np.sum(rays.flux)))
-    print("forward output flux = {}".format(np.sum(rForward.flux)))
-    print("reverse output flux = {}".format(np.sum(rReverse.flux)))
+    forwardFlux = np.sum(sum(rr.flux) for rr in rForward)
+    reverseFlux = np.sum(sum(rr.flux) for rr in rReverse)
+    print("forward output flux = {}".format(forwardFlux))
+    print("reverse output flux = {}".format(reverseFlux))
     print("destroyed flux      = {}".format(
-        np.sum(rays.flux) - np.sum(rForward.flux) - np.sum(rReverse.flux))
-    )
+        np.sum(rays.flux) - forwardFlux - reverseFlux
+    ))
 
+    x = np.concatenate([rr.x for rr in rForward])
+    y = np.concatenate([rr.y for rr in rForward])
+    flux = np.concatenate([rr.flux for rr in rForward])
 
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 8))
     ax.hexbin(
-        rForward.x*factor, rForward.y*factor, rForward.flux,
+        x*factor, y*factor, flux,
         reduce_C_function=np.sum, vmax=4e-2, gridsize=1000, bins='log'
     )
     ax.get_xaxis().set_visible(False)
