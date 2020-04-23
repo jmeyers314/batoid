216dc2d7c5953610d56cee39761048a30501b15d
Josh Meyers <jmeyers314@gmail.com>
2020-04-23 12:44:34 -0700

    Add path kwarg to traceFull

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
+        "--slow", action="store_true", default=False, help="run slow tests"
     )
 
 
@@ -12,10 +12,10 @@ def pytest_configure(config):
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
+    if config.getoption("--slow"):
+        # --slow given in cli: do not skip slow tests
         return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    skip_slow = pytest.mark.skip(reason="need --slow option to run")
     for item in items:
         if "slow" in item.keywords:
             item.add_marker(skip_slow)
