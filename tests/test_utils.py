d1fdd706493839e56169b803aa063683f25cbdb4
Josh Meyers <jmeyers314@gmail.com>
2020-09-08 22:59:51 -0700

    Revive utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,17 +2,16 @@ import batoid
 from test_helpers import timer
 
 import numpy as np
-import pytest
 
 
 @timer
 def test_normalized():
-    np.random.seed(5)
+    rng = np.random.default_rng(5)
     for _ in range(1000):
-        x = np.random.uniform()
-        y = np.random.uniform()
-        z = np.random.uniform()
-        w = np.random.uniform()
+        x = rng.uniform()
+        y = rng.uniform()
+        z = rng.uniform()
+        w = rng.uniform()
 
         np.testing.assert_allclose(
             np.linalg.norm(batoid.utils.normalized([x])),
@@ -38,9 +37,9 @@ def test_normalized():
 
 @timer
 def test_gnomonicDirCos():
-    np.random.seed(57)
-    u = np.random.uniform(-0.5, 0.5, size=10000)
-    v = np.random.uniform(-0.5, 0.5, size=10000)
+    rng = np.random.default_rng(57)
+    u = rng.uniform(-0.5, 0.5, size=10000)
+    v = rng.uniform(-0.5, 0.5, size=10000)
 
     # Test round trip
     u1, v1 = batoid.utils.dirCosToGnomonic(*batoid.utils.gnomonicToDirCos(u, v))
@@ -48,8 +47,8 @@ def test_gnomonicDirCos():
     np.testing.assert_allclose(v, v1, rtol=1e-10, atol=1e-12)
 
     # Test round trip in the other direction
-    alpha = np.random.uniform(-0.1, 0.1, size=10000)
-    beta = np.random.uniform(-0.1, 0.1, size=10000)
+    alpha = rng.uniform(-0.1, 0.1, size=10000)
+    beta = rng.uniform(-0.1, 0.1, size=10000)
     gamma = -np.sqrt(1 - alpha**2 - beta**2)
     alpha1, beta1, gamma1 = batoid.utils.gnomonicToDirCos(
         *batoid.utils.dirCosToGnomonic(alpha, beta, gamma)
@@ -59,21 +58,25 @@ def test_gnomonicDirCos():
     np.testing.assert_allclose(gamma, gamma1, rtol=1e-10, atol=1e-12)
 
     # For really tiny angles, u/v should be basically the same as alpha/beta
-    u = np.random.uniform(-1e-6, 1e-6, size=10000)
-    v = np.random.uniform(-1e-6, 1e-6, size=10000)
+    u = rng.uniform(-1e-6, 1e-6, size=10000)
+    v = rng.uniform(-1e-6, 1e-6, size=10000)
     alpha, beta, gamma = batoid.utils.gnomonicToDirCos(u, v)
     np.testing.assert_allclose(alpha, u, rtol=0, atol=1e-8)
     np.testing.assert_allclose(beta, v, rtol=0, atol=1e-8)
 
     # Check normalization of direction cosines
-    np.testing.assert_allclose(np.sqrt(alpha*alpha+beta*beta+gamma*gamma), 1, rtol=0, atol=1e-15)
+    np.testing.assert_allclose(
+        np.sqrt(alpha*alpha + beta*beta + gamma*gamma),
+        1,
+        rtol=0, atol=1e-15
+    )
 
 
 @timer
 def test_postelDirCos():
-    np.random.seed(577)
-    u = np.random.uniform(-0.5, 0.5, size=10000)
-    v = np.random.uniform(-0.5, 0.5, size=10000)
+    rng = np.random.default_rng(577)
+    u = rng.uniform(-0.5, 0.5, size=10000)
+    v = rng.uniform(-0.5, 0.5, size=10000)
 
     # Test round trip
     u1, v1 = batoid.utils.dirCosToPostel(*batoid.utils.postelToDirCos(u, v))
@@ -81,8 +84,8 @@ def test_postelDirCos():
     np.testing.assert_allclose(v, v1, rtol=1e-10, atol=1e-12)
 
     # Test round trip in the other direction
-    alpha = np.random.uniform(-0.1, 0.1, size=10000)
-    beta = np.random.uniform(-0.1, 0.1, size=10000)
+    alpha = rng.uniform(-0.1, 0.1, size=10000)
+    beta = rng.uniform(-0.1, 0.1, size=10000)
     gamma = -np.sqrt(1 - alpha**2 - beta**2)
     alpha1, beta1, gamma1 = batoid.utils.postelToDirCos(
         *batoid.utils.dirCosToPostel(alpha, beta, gamma)
@@ -92,21 +95,25 @@ def test_postelDirCos():
     np.testing.assert_allclose(gamma, gamma1, rtol=1e-10, atol=1e-12)
 
     # For really tiny angles, u/v should be basically the same as alpha/beta
-    u = np.random.uniform(-1e-6, 1e-6, size=10000)
-    v = np.random.uniform(-1e-6, 1e-6, size=10000)
+    u = rng.uniform(-1e-6, 1e-6, size=10000)
+    v = rng.uniform(-1e-6, 1e-6, size=10000)
     alpha, beta, gamma = batoid.utils.postelToDirCos(u, v)
     np.testing.assert_allclose(alpha, u, rtol=0, atol=1e-8)
     np.testing.assert_allclose(beta, v, rtol=0, atol=1e-8)
 
     # Check normalization of direction cosines
-    np.testing.assert_allclose(np.sqrt(alpha*alpha+beta*beta+gamma*gamma), 1, rtol=0, atol=1e-15)
+    np.testing.assert_allclose(
+        np.sqrt(alpha*alpha + beta*beta + gamma*gamma),
+        1,
+        rtol=0, atol=1e-15
+    )
 
 
 @timer
 def test_zemaxDirCos():
-    np.random.seed(5772)
-    u = np.random.uniform(-0.5, 0.5, size=10000)
-    v = np.random.uniform(-0.5, 0.5, size=10000)
+    rng = np.random.default_rng(5772)
+    u = rng.uniform(-0.5, 0.5, size=10000)
+    v = rng.uniform(-0.5, 0.5, size=10000)
 
     # Test round trip
     u1, v1 = batoid.utils.dirCosToZemax(*batoid.utils.zemaxToDirCos(u, v))
@@ -114,8 +121,8 @@ def test_zemaxDirCos():
     np.testing.assert_allclose(v, v1, rtol=1e-10, atol=1e-12)
 
     # Test round trip in the other direction
-    alpha = np.random.uniform(-0.1, 0.1, size=10000)
-    beta = np.random.uniform(-0.1, 0.1, size=10000)
+    alpha = rng.uniform(-0.1, 0.1, size=10000)
+    beta = rng.uniform(-0.1, 0.1, size=10000)
     gamma = -np.sqrt(1 - alpha**2 - beta**2)
     alpha1, beta1, gamma1 = batoid.utils.zemaxToDirCos(
         *batoid.utils.dirCosToZemax(alpha, beta, gamma)
@@ -125,30 +132,36 @@ def test_zemaxDirCos():
     np.testing.assert_allclose(gamma, gamma1, rtol=1e-10, atol=1e-12)
 
     # For really tiny angles, u/v should be basically the same as alpha/beta
-    u = np.random.uniform(-1e-6, 1e-6, size=10000)
-    v = np.random.uniform(-1e-6, 1e-6, size=10000)
+    u = rng.uniform(-1e-6, 1e-6, size=10000)
+    v = rng.uniform(-1e-6, 1e-6, size=10000)
     alpha, beta, gamma = batoid.utils.zemaxToDirCos(u, v)
     np.testing.assert_allclose(alpha, u, rtol=0, atol=1e-8)
     np.testing.assert_allclose(beta, v, rtol=0, atol=1e-8)
 
     # Check normalization of direction cosines
-    np.testing.assert_allclose(np.sqrt(alpha*alpha+beta*beta+gamma*gamma), 1, rtol=0, atol=1e-15)
+    np.testing.assert_allclose(
+        np.sqrt(alpha*alpha + beta*beta + gamma*gamma),
+        1,
+        rtol=0, atol=1e-15
+    )
 
 
 @timer
 def test_stereographicDirCos():
-    np.random.seed(57721)
-    u = np.random.uniform(-0.5, 0.5, size=10000)
-    v = np.random.uniform(-0.5, 0.5, size=10000)
+    rng = np.random.default_rng(57721)
+    u = rng.uniform(-0.5, 0.5, size=10000)
+    v = rng.uniform(-0.5, 0.5, size=10000)
 
     # Test round trip
-    u1, v1 = batoid.utils.dirCosToStereographic(*batoid.utils.stereographicToDirCos(u, v))
+    u1, v1 = batoid.utils.dirCosToStereographic(
+        *batoid.utils.stereographicToDirCos(u, v)
+    )
     np.testing.assert_allclose(u, u1, rtol=1e-10, atol=1e-12)
     np.testing.assert_allclose(v, v1, rtol=1e-10, atol=1e-12)
 
     # Test round trip in the other direction
-    alpha = np.random.uniform(-0.1, 0.1, size=10000)
-    beta = np.random.uniform(-0.1, 0.1, size=10000)
+    alpha = rng.uniform(-0.1, 0.1, size=10000)
+    beta = rng.uniform(-0.1, 0.1, size=10000)
     gamma = np.sqrt(1 - alpha**2 - beta**2)
     alpha1, beta1, gamma1 = batoid.utils.stereographicToDirCos(
         *batoid.utils.dirCosToStereographic(alpha, beta, gamma)
@@ -158,30 +171,36 @@ def test_stereographicDirCos():
     np.testing.assert_allclose(gamma, gamma1, rtol=1e-10, atol=1e-12)
 
     # For really tiny angles, u/v should be basically the same as alpha/beta
-    u = np.random.uniform(-1e-6, 1e-6, size=10000)
-    v = np.random.uniform(-1e-6, 1e-6, size=10000)
+    u = rng.uniform(-1e-6, 1e-6, size=10000)
+    v = rng.uniform(-1e-6, 1e-6, size=10000)
     alpha, beta, gamma = batoid.utils.stereographicToDirCos(u, v)
     np.testing.assert_allclose(alpha, u, rtol=0, atol=1e-8)
     np.testing.assert_allclose(beta, v, rtol=0, atol=1e-8)
 
     # Check normalization of direction cosines
-    np.testing.assert_allclose(np.sqrt(alpha*alpha+beta*beta+gamma*gamma), 1, rtol=0, atol=1e-15)
+    np.testing.assert_allclose(
+        np.sqrt(alpha*alpha + beta*beta + gamma*gamma),
+        1,
+        rtol=0, atol=1e-15
+    )
 
 
 @timer
 def test_orthographicDirCos():
-    np.random.seed(577215)
-    u = np.random.uniform(-0.5, 0.5, size=10000)
-    v = np.random.uniform(-0.5, 0.5, size=10000)
+    rng = np.random.default_rng(577215)
+    u = rng.uniform(-0.5, 0.5, size=10000)
+    v = rng.uniform(-0.5, 0.5, size=10000)
 
     # Test round trip
-    u1, v1 = batoid.utils.dirCosToOrthographic(*batoid.utils.orthographicToDirCos(u, v))
+    u1, v1 = batoid.utils.dirCosToOrthographic(
+        *batoid.utils.orthographicToDirCos(u, v)
+    )
     np.testing.assert_allclose(u, u1, rtol=1e-10, atol=1e-12)
     np.testing.assert_allclose(v, v1, rtol=1e-10, atol=1e-12)
 
     # Test round trip in the other direction
-    alpha = np.random.uniform(-0.1, 0.1, size=10000)
-    beta = np.random.uniform(-0.1, 0.1, size=10000)
+    alpha = rng.uniform(-0.1, 0.1, size=10000)
+    beta = rng.uniform(-0.1, 0.1, size=10000)
     gamma = -np.sqrt(1 - alpha**2 - beta**2)
     alpha1, beta1, gamma1 = batoid.utils.orthographicToDirCos(
         *batoid.utils.dirCosToOrthographic(alpha, beta, gamma)
@@ -191,21 +210,25 @@ def test_orthographicDirCos():
     np.testing.assert_allclose(gamma, gamma1, rtol=1e-10, atol=1e-12)
 
     # For really tiny angles, u/v should be basically the same as alpha/beta
-    u = np.random.uniform(-1e-6, 1e-6, size=10000)
-    v = np.random.uniform(-1e-6, 1e-6, size=10000)
+    u = rng.uniform(-1e-6, 1e-6, size=10000)
+    v = rng.uniform(-1e-6, 1e-6, size=10000)
     alpha, beta, gamma = batoid.utils.orthographicToDirCos(u, v)
     np.testing.assert_allclose(alpha, u, rtol=0, atol=1e-8)
     np.testing.assert_allclose(beta, v, rtol=0, atol=1e-8)
 
     # Check normalization of direction cosines
-    np.testing.assert_allclose(np.sqrt(alpha*alpha+beta*beta+gamma*gamma), 1, rtol=0, atol=1e-15)
+    np.testing.assert_allclose(
+        np.sqrt(alpha*alpha + beta*beta + gamma*gamma),
+        1,
+        rtol=0, atol=1e-15
+    )
 
 
 @timer
 def test_lambertDirCos():
-    np.random.seed(5772156)
-    u = np.random.uniform(-0.5, 0.5, size=10000)
-    v = np.random.uniform(-0.5, 0.5, size=10000)
+    rng = np.random.default_rng(5772156)
+    u = rng.uniform(-0.5, 0.5, size=10000)
+    v = rng.uniform(-0.5, 0.5, size=10000)
 
     # Test round trip
     u1, v1 = batoid.utils.dirCosToLambert(*batoid.utils.lambertToDirCos(u, v))
@@ -213,8 +236,8 @@ def test_lambertDirCos():
     np.testing.assert_allclose(v, v1, rtol=1e-10, atol=1e-12)
 
     # Test round trip in the other direction
-    alpha = np.random.uniform(-0.5, 0.5, size=10000)
-    beta = np.random.uniform(-0.5, 0.5, size=10000)
+    alpha = rng.uniform(-0.5, 0.5, size=10000)
+    beta = rng.uniform(-0.5, 0.5, size=10000)
     gamma = -np.sqrt(1 - alpha**2 - beta**2)
     alpha1, beta1, gamma1 = batoid.utils.lambertToDirCos(
         *batoid.utils.dirCosToLambert(alpha, beta, gamma)
@@ -225,33 +248,33 @@ def test_lambertDirCos():
     np.testing.assert_allclose(gamma, gamma1, rtol=1e-10, atol=1e-12)
 
     # For really tiny angles, u/v should be basically the same as alpha/beta
-    u = np.random.uniform(-1e-6, 1e-6, size=10000)
-    v = np.random.uniform(-1e-6, 1e-6, size=10000)
+    u = rng.uniform(-1e-6, 1e-6, size=10000)
+    v = rng.uniform(-1e-6, 1e-6, size=10000)
     alpha, beta, gamma = batoid.utils.lambertToDirCos(u, v)
     np.testing.assert_allclose(alpha, u, rtol=0, atol=1e-8)
     np.testing.assert_allclose(beta, v, rtol=0, atol=1e-8)
 
     # Check normalization of direction cosines
-    np.testing.assert_allclose(np.sqrt(alpha*alpha+beta*beta+gamma*gamma), 1, rtol=0, atol=1e-15)
+    np.testing.assert_allclose(
+        np.sqrt(alpha*alpha + beta*beta + gamma*gamma),
+        1,
+        rtol=0, atol=1e-15
+    )
 
 
-hasCoord = True
-try:
-    import coord
-except ImportError:
-    hasCoord = False
-
-
-@pytest.mark.skipif(not hasCoord, reason="LSSTDESC.coord not found")
 @timer
 def test_coord():
+    rng = np.random.default_rng(57721566)
+    import coord
     pole = coord.CelestialCoord(0.*coord.degrees, 90.*coord.degrees)
-    u = np.random.uniform(-0.5, 0.5, size=10000)
-    v = np.random.uniform(-0.5, 0.5, size=10000)
+    u = rng.uniform(-0.5, 0.5, size=10000)
+    v = rng.uniform(-0.5, 0.5, size=10000)
 
     for projection in ['gnomonic', 'stereographic', 'postel', 'lambert']:
         ra, dec = pole.deproject_rad(u, v, projection=projection)
-        xcos, ycos, zcos = batoid.utils.fieldToDirCos(u, v, projection=projection)
+        xcos, ycos, zcos = batoid.utils.fieldToDirCos(
+            u, v, projection=projection
+        )
         np.testing.assert_allclose(-np.sin(dec), zcos, rtol=0, atol=1e-13)
         np.testing.assert_allclose(
             np.abs((np.pi/2-ra)-np.arctan2(ycos, xcos)),
