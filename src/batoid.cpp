25b2deac7e14144578073f583f803d1d9b7745aa
Josh Meyers <jmeyers314@gmail.com>
2021-11-15 16:05:45 -0800

    Initialize dt everywhere

--- src/batoid.cpp
+++ src/batoid.cpp
@@ -218,7 +218,7 @@ namespace batoid {
             double t = tptr[i];
             // intersection
             if (!failptr[i]) {
-                double dt;
+                double dt = 0.0;
                 bool success = surfacePtr->timeToIntersect(x, y, z, vx, vy, vz, dt);
                 if (success) {
                     x += vx * dt;
@@ -310,7 +310,7 @@ namespace batoid {
             double t = tptr[i];
             if (!failptr[i]) {
                 // intersection
-                double dt;
+                double dt = 0.0;
                 bool success = surfacePtr->timeToIntersect(x, y, z, vx, vy, vz, dt);
                 if (success) {
                     // propagation
@@ -414,7 +414,7 @@ namespace batoid {
             double t = tptr[i];
             if (!failptr[i]) {
                 // intersection
-                double dt;
+                double dt = 0.0;
                 bool success = surfacePtr->timeToIntersect(x, y, z, vx, vy, vz, dt);
                 if (success) {
                     // propagation
@@ -546,7 +546,7 @@ namespace batoid {
             double t = tptr[i];
             if (!failptr[i]) {
                 // intersection
-                double dt;
+                double dt = 0.0;
                 bool success = surfacePtr->timeToIntersect(x, y, z, vx, vy, vz, dt);
                 if (success) {
                     // propagation
@@ -671,7 +671,7 @@ namespace batoid {
             double t = tptr[i];
             if (!failptr[i]) {
                 // intersection
-                double dt=0.0;
+                double dt = 0.0;
                 bool success = surfacePtr->timeToIntersect(x, y, z, vx, vy, vz, dt);
                 if (success) {
                     // propagation
