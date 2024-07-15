bce8591d090d15a07962ac248d17259ce5724e15
Josh Meyers <jmeyers314@gmail.com>
2024-07-15 13:31:44 -0700

    Reduce parallelism from 16->8 to reduce RAM consumption during build

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class CMakeBuild(build_ext):
         build_args = ['--config', cfg]
 
         cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-        build_args += ['--', '-j16']
+        build_args += ['--', '-j8']
 
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
